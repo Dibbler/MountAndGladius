ce7a18eb837af29c96248423f98e3ad54b59a94f
Wibbler <zaqduu@gmail.com>
2020-04-11 22:25:56 +0100

    ModuleData xml copying
    ps1 script now copies ModuleData xml's to the module directory

--- DeployFiles.ps1
+++ DeployFiles.ps1
@@ -1,11 +1,16 @@
 $destination = "C:\Program Files\Steam\steamapps\common\Mount & Blade II Bannerlord\Modules\MountAndGladius\"
-$binaryDir = ($destination + "\bin\Win64_Shipping_Client")
+$binaryDir = ($destination + "bin\Win64_Shipping_Client")
+$xmlDir = ($destination + "ModuleData")
 
 MD $destination -Force
 MD $binaryDir -Force
+MD $xmlDir -Force
 
 #Copy built files
 Copy-Item "*" -Include "*.dll", "*.pdb" -Destination $binaryDir -Recurse -Force
 
 #Copy SubModule config
 Copy-Item -Path "..\..\SubModule.xml" -Destination $destination -Force
+
+#Copy ModuleData
+Copy-Item -Path "..\..\ModuleData\*" -Include "*.xml" -Destination $xmlDir -Force
